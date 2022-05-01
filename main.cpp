c5c41d43c17d84a51bc6f57a1d13b1f46f4f4790
lamho866 <s1071547@mail.yzu.edu.tw>
2022-05-01 15:49:30 +0800

    Change to Boost

--- main.cpp
+++ main.cpp
@@ -1,146 +1,159 @@
-#include <bits/stdc++.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdio.h>
+#include <fstream>
+#include <boost/assign.hpp>
+
+#include <boost/algorithm/string.hpp>
+#include <boost/geometry/geometry.hpp>
+#include <boost/geometry/io/svg/write_svg.hpp>
+#include <boost/geometry/geometries/geometries.hpp>
+#include <boost/geometry/algorithms/envelope.hpp>
+
 using namespace std;
 
-void readLine(char *s){
-    char tap[20];
-    double x1, x2, y1, y2;
-    sscanf(s + 5,"%lf,%lf,%lf,%lf", &x1, &y1, &x2, &y2);
-    printf("line: %lf,%lf,%lf,%lf\n", x1, y1, x2, y2);
-}
+namespace bg = boost::geometry;
+typedef bg::model::d2::point_xy<double> BoostPoint;
+typedef bg::model::linestring<BoostPoint> BoostLineString;
+typedef bg::model::polygon<BoostPoint> BoostPolygon;
+typedef bg::model::multi_polygon<BoostPolygon> BoostMultipolygon;
 
-void readarc(char *s){
-    char tap[20];
-    double x1, x2, y1, y2, rx, ry;
-    sscanf(s + 4,"%lf,%lf,%lf,%lf,%lf,%lf,%s", &x1, &y1, &x2, &y2, &rx, &ry, tap);
-    printf("arc: %lf,%lf,%lf,%lf,%lf,%lf, %s\n", x1, y1, x2, y2, rx, ry, tap);
-}
+#define points_per_circle 36
+boost::geometry::strategy::buffer::join_round join_strategy(points_per_circle);
+boost::geometry::strategy::buffer::end_round end_strategy(points_per_circle);
+boost::geometry::strategy::buffer::point_circle circle_strategy(points_per_circle);
+boost::geometry::strategy::buffer::side_straight side_strategy;
 
-class Line
+class Cycle
 {
-public:
-    bool isLine, isCW; //CW or CCW 
-    double x1, y1, x2, y2, rx, ry;
-    char tap[5];
+public: //CW or CCW 
+	double x1, y1, x2, y2, rx, ry;
+	Cycle(double _x1, double _y1, double _x2, double _y2, double _rx, double _ry)
+		:x1(_x1), y1(_y1), x2(_x2), y2(_y2), rx(_rx), ry(_ry){}
 
-    Line(bool _isLine, double _x1, double _y1, double _x2, double _y2, double _rx = 0.0, double _ry = 0.0, bool _isCW = false)
-     : isLine(_isLine), x1(_x1), y1(_y1), x2(_x2), y2(_y2), rx(_rx), ry(_ry), isCW(_isCW)
-     {}
-
-    double dist(){
-        return sqrt((x1 - x2)*(x1 - x2) + (y1 -y2)*(y1 - y2));
-    }
 };
 
-void connectLineAB(Line &a, Line &b){
-    double a1, b1, c1, a2, b2, c2;
-    double D, Dx, Dy;
-    a1 = a.y1 - a.y2, b1 = -a.x1 + a.x2;
-    a2 = b.y1 - b.y2, b2 = -b.x1 + b.x2;
-    c1 = a1 * a.x1 + b1 * a.y1;
-    c2 = a2 * b.x1 + b2 * b.y1;
-
-    D = a1 * b2 - a2 * b1;
-    Dx = c1 * b2 - c2 * b1;
-    Dy = a1 * c2 - a2 * c1;
-
-    a.x2 = b.x1 = Dx / D;
-    a.y2 = b.y1 = Dy / D;
-}
-
-
-class Polygom{
+class Polygom {
 public:
-    vector<Line> shape;
-    void addLine(char *s){
-        char tap[20];
-        double x1, x2, y1, y2, rx, ry;
-        if(s[0] == 'l'){
-            //isLine
-            sscanf(s + 5,"%lf,%lf,%lf,%lf", &x1, &y1, &x2, &y2);
-            shape.push_back(Line(true, x1, y1, x2, y2));
-            printf("line: %lf,%lf,%lf,%lf\n", x1, y1, x2, y2);
-            
-        }
-        if(s[0] == 'a'){
-            //isCycle
-            sscanf(s + 4,"%lf,%lf,%lf,%lf,%lf,%lf,%s", &x1, &y1, &x2, &y2, &rx, &ry, tap);
-            shape.push_back(Line(false, x1, y1, x2, y2, rx, ry, strcmp(tap, "CW") == 0));
-            printf("arc: %lf,%lf,%lf,%lf,%lf,%lf, ", x1, y1, x2, y2, rx, ry);
-            if(strcmp(tap, "CW") == 0) printf("CW\n");
-            else printf("CWW\n");
-        }
-    }
-    Polygom(){}
+	vector<Cycle> cw;
+	vector<Cycle> ccw;
+	string shape;
+	void addLine(string &s) {
+		if (s[0] == 'l') {
+			//isLine
+			string subS = s.substr(5);
+			int i = 0, j = subS.size() - 1;
+			while (subS[i] != ',') ++i; subS[i] = ' ';
+			while (subS[j] != ',') --j; subS[j] = ' ';
+			shape += subS + ',';
+		}
+		if (s[0] == 'a') {
+			for (int i = 0; i < s.size(); ++i)
+				if (s[i] == ',')
+					s[i] = ' ';
+			char x1[10], x2[10], rx[10], y1[10], y2[10], ry[10], tap[10];
+			sscanf(s.c_str() + 4, "%s %s %s %s %s %s %s", x1, y1, x2, y2, rx, ry, tap);
+			shape += boost::lexical_cast<std::string>(x1) + ' ' + boost::lexical_cast<std::string>(y1) + ',' + boost::lexical_cast<std::string>(x2) + ' ' + boost::lexical_cast<std::string>(y2) + ',';
+			if (strcmp(tap, "CW") == 0)
+				cw.push_back(Cycle(atof(x1), atof(y1), atof(x2), atof(y2), atof(rx), atof(ry)));
+			else 
+				ccw.push_back(Cycle(atof(x1), atof(y1), atof(x2), atof(y2), atof(rx), atof(ry)));
+		}
+		
+	}
+	Polygom() {}
 };
 
-void addOutLine(Polygom &outline, Line &a, Line &b, double assemblygap){
-    double addX, addY;
-    addX = -(a.y2 - a.y1) / a.dist() * assemblygap;
-    addY = (a.x2 - a.x1) / a.dist() * assemblygap;
-    outline.shape.push_back(Line(true, a.x1 + addX, a.y1 + addY, a.x2 + addX, a.y2 + addY));
+void makeCycle(Cycle &cycle, BoostMultipolygon &multiPolygonCycle) {
+	BoostPoint pointA(cycle.x1, cycle.y1);
+	BoostPoint centerCycle(cycle.rx, cycle.ry);
+
+	double dist = bg::distance(pointA, centerCycle);
+	boost::geometry::strategy::buffer::distance_symmetric<double> distance_strategy(dist);
+	boost::geometry::buffer(centerCycle, multiPolygonCycle, distance_strategy, side_strategy, join_strategy, end_strategy, circle_strategy);
 }
 
-void printPolyShape(Polygom &poly, string s){
-    printf("%s_x = [", s.c_str());
-    for(int i = 0; i < poly.shape.size(); ++i)
-        printf("%lf, %lf, ", poly.shape[i].x1, poly.shape[i].x2);
-    printf("]\n%s_y = [", s.c_str()) ;
-    for(int i = 0; i  < poly.shape.size(); ++i)
-        printf("%lf, %lf, ", poly.shape[i].y1, poly.shape[i].y2);
-    printf("]\n");
+void makeThePolygonShape(BoostPolygon &bgPlogom, Polygom &polyShape) {
+	BoostMultipolygon unionCycle;
+	BoostMultipolygon diffCycle;
+	vector<BoostPolygon> result;
+
+	polyShape.shape[polyShape.shape.size() - 1] = ' ';
+	bg::read_wkt("POLYGON((" + polyShape.shape + "))", bgPlogom);
+
+	//different part
+	for (int i = 0; i < polyShape.ccw.size(); ++i) {
+		makeCycle(polyShape.ccw[i], diffCycle);
+		bg::difference(bgPlogom, diffCycle.front(), result);
+		if(result.size() > 0)
+			bgPlogom = result[0];
+	}
+	
+	for (int i = 0; i < polyShape.cw.size(); ++i) {
+		makeCycle(polyShape.ccw[i], unionCycle);
+		bg::union_(bgPlogom, diffCycle.front(), result);
+		if (result.size() > 0)
+			bgPlogom = result[0];
+	}
 }
 
-void drawThePolygonOffsetting(Polygom &assembly, double const assemblygap){
-    Polygom outline;
 
-    for(int i = 0; i < assembly.shape.size() - 1; ++i)
-        addOutLine(outline, assembly.shape[i], assembly.shape[i + 1], assemblygap);
-    addOutLine(outline, assembly.shape[assembly.shape.size() - 1], assembly.shape[0], assemblygap);
+int main()
+{
+	Polygom assembly;
+	BoostPolygon bgAssembly;
 
-    //connect the line
-    for(int i = 0; i < outline.shape.size() - 1; ++i)
-        connectLineAB(outline.shape[i], outline.shape[i + 1]);
-    connectLineAB(outline.shape[outline.shape.size() - 1], outline.shape[0]);
+	vector<Polygom> cropperList;
+	BoostMultipolygon multBGCropper;
+	BoostPolygon cropper;
 
-    printPolyShape(outline, "new");
-}
+	string str;
+	double assemblygap, croppergap, silkscreenlen;
+	int cropSize = 0;
 
-int main(){
-    Polygom assembly;
-    vector<Polygom> cropperList;
-    int cropSize = 0;
+	std::ifstream input("Problem.txt");
+	input >> str;
+	assemblygap = atof(str.substr(12).c_str());
+	//printf("assemblygap: %lf\n", assemblygap);
+	input >> str;
+	//sscanf(str, "%lf", &croppergap);
+	input >> str;
+	//sscanf(str, "%lf", &silkscreenlen);
+	input >> str;
 
-    char str[256];
-    double assemblygap, croppergap, silkscreenlen;
-    cin >> str;
-    sscanf(str + 12, "%lf", &assemblygap);
-    cin >> str;
-    sscanf(str , "%lf", &croppergap);
-    cin >> str;
-    sscanf(str, "%lf", &silkscreenlen);
-    cin >> str;
-    //read assembly
-    printf("assembly\n");
-    while(1){
-        cin >> str;
-        if(str[0] == 'c') break;
-        assembly.addLine(str); 
-    }
-    
-    //read cropperList
-    printf("copper\n");
-    cropperList.push_back(Polygom());
-    while(cin >> str){
-        if(str[0] == 'c'){
-            printf("copper\n");
-            cropperList.push_back(Polygom());
-            cropSize++;
-            continue;
-        }
-        cropperList[cropSize].addLine(str);
-    }
+	double x, y;
+	//assembly
+	while (1) {
+		input >> str;
+		if (str[0] == 'c') break;
+		assembly.addLine(str);
+	}
+	makeThePolygonShape(bgAssembly, assembly);
 
-    printPolyShape(assembly, "org");
-    printf("assemblygap : %lf\n", assemblygap);
-    drawThePolygonOffsetting(assembly, assemblygap);
+	//cropper
+	cropperList.push_back(Polygom());
+	while (input >> str) {
+		if (str[0] == 'c') {
+			makeThePolygonShape(cropper, cropperList[cropSize]);
+			multBGCropper.push_back(cropper);
+			cropperList.push_back(Polygom());
+			cropSize++;
+			continue;
+		}
+		cropperList[cropSize].addLine(str);
+	}
+	makeThePolygonShape(cropper, cropperList[cropSize]);
+	multBGCropper.push_back(cropper);
+	cropSize++;
+	{
+		std::ofstream svg("output.svg");
+		boost::geometry::svg_mapper<BoostPoint> mapper(svg, 400, 400);
+		mapper.add(bgAssembly);
+		mapper.add(multBGCropper);
+
+		mapper.map(bgAssembly, "fill-opacity:0.5;fill:rgb(153,204,0);stroke:rgb(153,204,0);stroke-width:2");
+		mapper.map(multBGCropper, "fill-opacity:0.5;fill:rgb(204,153,0);stroke:rgb(202,153,0);stroke-width:2");
+	}
 }
