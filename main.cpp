d7317ffc95be05acf71aef664121f296e5498e2e
lamho866 <72925954+lamho866@users.noreply.github.com>
2022-05-16 22:19:49 +0800

    [SCDO_4] Buffer the assembly like a outline (#4)
    * [SCDO_4] add the line buffer function and diference the multiCropper
    	buildAssemblyLine:
    		1. Buffer the assembly line
    		2. Buffer the cropper
    		3. silkscreen = Buffered_assembly_line -
    		   Buffered_cropper
    
    Co-authored-by: lamho866 <s1071547@mail.yzu.edu.tw>

--- main.cpp
+++ main.cpp
@@ -12,6 +12,8 @@
 #include <boost/geometry/geometries/geometries.hpp>
 #include <boost/geometry/algorithms/envelope.hpp>
 
+#include <boost/foreach.hpp>
+
 using namespace std;
 #define PI 3.14159265
 
@@ -20,12 +22,14 @@ typedef bg::model::d2::point_xy<double> BoostPoint;
 typedef bg::model::linestring<BoostPoint> BoostLineString;
 typedef bg::model::polygon<BoostPoint> BoostPolygon;
 typedef bg::model::multi_polygon<BoostPolygon> BoostMultipolygon;
+typedef bg::model::multi_linestring<BoostLineString> BoostMultiLineString;
 
-#define points_per_circle 36
-boost::geometry::strategy::buffer::join_round join_strategy(points_per_circle);
-boost::geometry::strategy::buffer::end_round end_strategy(points_per_circle);
-boost::geometry::strategy::buffer::point_circle circle_strategy(points_per_circle);
+//define the strategies
+const int points_per_circle = 36;
+boost::geometry::strategy::buffer::join_miter join_strategy;
+boost::geometry::strategy::buffer::end_flat end_strategy;
 boost::geometry::strategy::buffer::side_straight side_strategy;
+boost::geometry::strategy::buffer::point_circle point_strategy;
 
 class Cycle
 {
@@ -83,21 +87,22 @@ public:
 	vector<Cycle> ccw;
 	string shape;
 	void addLine(string &s) {
+		char x1[10], x2[10], rx[10], y1[10], y2[10], ry[10], tap[10];
+		for (int i = 0; i < s.size(); ++i)
+			if (s[i] == ',')
+				s[i] = ' ';
 		if (s[0] == 'l') {
 			//isLine
-			string subS = s.substr(5);
-			int i = 0, j = subS.size() - 1;
-			while (subS[i] != ',') ++i; subS[i] = ' ';
-			while (subS[j] != ',') --j; subS[j] = ' ';
-			shape += subS + ',';
+			sscanf(s.c_str() + 5, "%s %s %s %s\n", x1, y1, x2, y2);
+			//printf("%s %s %s %s\n", x1, y1, x2, y2);
+			shape += boost::lexical_cast<std::string>(x1) + ' ' + boost::lexical_cast<std::string>(y1) + ',' + boost::lexical_cast<std::string>(x2) + ' ' + boost::lexical_cast<std::string>(y2) + ',';
 		}
 		if (s[0] == 'a') {
 			for (int i = 0; i < s.size(); ++i)
 				if (s[i] == ',')
 					s[i] = ' ';
-			char x1[10], x2[10], rx[10], y1[10], y2[10], ry[10], tap[10];
 			sscanf(s.c_str() + 4, "%s %s %s %s %s %s %s", x1, y1, x2, y2, rx, ry, tap);
-			///printf("arc: %s %s %s %s %s %s %s ", x1, y1, x2, y2, rx, ry);
+			//printf("arc: %s %s %s %s %s %s %s\n", x1, y1, x2, y2, rx, ry);
 			shape += boost::lexical_cast<std::string>(x1) + ' ' + boost::lexical_cast<std::string>(y1) + ',';
 			Cycle c(atof(x1), atof(y1), atof(x2), atof(y2), atof(rx), atof(ry));
 
@@ -105,35 +110,55 @@ public:
 			bool isCW = strcmp(tap, "CW") == 0;
 			double maxDeg = c.deg(isCW);
 			curX = c.x1, curY = c.y1;
+			shape += boost::lexical_cast<std::string>(x1) + ' ' + boost::lexical_cast<std::string>(y1) + ',';
 			for (double param = c.rDegSt; param < maxDeg - c.rDegSt; param += c.rDegSt) {
 				c.rotationPt(curX, curY, nxtX, nxtY, isCW);
 				shape += boost::lexical_cast<std::string>(nxtX) + ' ' + boost::lexical_cast<std::string>(nxtY) + ',';
 				curX = nxtX, curY = nxtY;
 			}
+			shape += boost::lexical_cast<std::string>(x2) + ' ' + boost::lexical_cast<std::string>(y2) + ',';
 		}
 
 	}
 	Polygom() {}
 };
 
-void makeCycle(Cycle &cycle, BoostMultipolygon &multiPolygonCycle) {
-	BoostPoint pointA(cycle.x1, cycle.y1);
-	BoostPoint centerCycle(cycle.rx, cycle.ry);
-
-	double dist = bg::distance(pointA, centerCycle);
-	boost::geometry::strategy::buffer::distance_symmetric<double> distance_strategy(dist);
-	boost::geometry::buffer(centerCycle, multiPolygonCycle, distance_strategy, side_strategy, join_strategy, end_strategy, circle_strategy);
-}
-
-void makeThePolygonShape(BoostPolygon &bgPlogom, Polygom &polyShape) {
-	BoostMultipolygon unionCycle;
-	BoostMultipolygon diffCycle;
-	vector<BoostPolygon> result;
-
+void makeThePolygonShape(Polygom &polyShape, BoostPolygon &bgPlogom) {
 	polyShape.shape[polyShape.shape.size() - 1] = ' ';
+	//printf("%s\n", polyShape.shape.c_str());
 	bg::read_wkt("POLYGON((" + polyShape.shape + "))", bgPlogom);
 }
 
+void makeLine(Polygom &polyShape, BoostLineString &bgLineStr) {
+	polyShape.shape[polyShape.shape.size() - 1] = ' ';
+	//printf("%s\n", polyShape.shape.c_str());
+	bg::read_wkt("LINESTRING(" + polyShape.shape + ")", bgLineStr);
+}
+
+void buildAssemblyLine(Polygom &assembly, const double assemblygap, BoostMultiLineString multiCropperLs, const double croppergap, BoostMultipolygon &cropperMulLsBuffer, vector<BoostLineString> &bgDiff){
+	BoostMultiLineString assemblyMultLine;
+	BoostLineString assemblyLs;
+	makeLine(assembly, assemblyLs);
+	//make the outline
+	BoostMultipolygon assemblyOutLs;
+	bg::strategy::buffer::distance_symmetric<double> assemblygap_dist_strategy(assemblygap / 10.0);
+	boost::geometry::buffer(assemblyLs, assemblyOutLs, assemblygap_dist_strategy, side_strategy, join_strategy, end_strategy, point_strategy);
+
+	assert(assemblyOutLs.size() == 1);
+	string strLs = boost::lexical_cast<std::string>(bg::wkt(assemblyOutLs.front()));
+	string outLineStrLs = strLs.substr(9, strLs.find("),(") - 9);
+	bg::read_wkt("LINESTRING(" + outLineStrLs + ")", assemblyLs);
+	assemblyMultLine.push_back(assemblyLs);
+
+	//Draw the multCropperOutline
+	BoostMultiLineString cropperMultLine;
+	BoostLineString cropperLs;
+	bg::strategy::buffer::distance_symmetric<double> cropper_dist_strategy(croppergap);
+	boost::geometry::buffer(multiCropperLs, cropperMulLsBuffer, cropper_dist_strategy, side_strategy, join_strategy, end_strategy, point_strategy);
+	
+	//difference
+	bg::difference(assemblyMultLine, cropperMulLsBuffer, bgDiff);
+}
 
 int main()
 {
@@ -142,7 +167,12 @@ int main()
 
 	vector<Polygom> cropperList;
 	BoostMultipolygon multBGCropper;
+	BoostMultipolygon cropperMulLsBuffer;//
+	BoostMultiLineString multiCropperLs;
 	BoostPolygon cropper;
+	BoostLineString cropperLs;
+
+	vector<BoostLineString> bgDiff;
 
 	string str;
 	double assemblygap, croppergap, silkscreenlen;
@@ -153,6 +183,7 @@ int main()
 	assemblygap = atof(str.substr(12).c_str());
 	//printf("assemblygap: %lf\n", assemblygap);
 	input >> str;
+	croppergap = atof(str.substr(10).c_str());
 	//sscanf(str, "%lf", &croppergap);
 	input >> str;
 	//sscanf(str, "%lf", &silkscreenlen);
@@ -165,30 +196,46 @@ int main()
 		if (str[0] == 'c') break;
 		assembly.addLine(str);
 	}
-	makeThePolygonShape(bgAssembly, assembly);
+	makeThePolygonShape(assembly, bgAssembly);
 
 	//cropper
 	cropperList.push_back(Polygom());
 	while (input >> str) {
 		if (str[0] == 'c') {
-			makeThePolygonShape(cropper, cropperList[cropSize]);
+			makeThePolygonShape(cropperList[cropSize], cropper);
+			makeLine(cropperList[cropSize], cropperLs);
 			multBGCropper.push_back(cropper);
+			multiCropperLs.push_back(cropperLs);
+			cropper.clear();
 			cropperList.push_back(Polygom());
 			cropSize++;
 			continue;
 		}
 		cropperList[cropSize].addLine(str);
 	}
-	makeThePolygonShape(cropper, cropperList[cropSize]);
+	makeThePolygonShape(cropperList[cropSize], cropper);
+	makeLine(cropperList[cropSize], cropperLs);
 	multBGCropper.push_back(cropper);
+	multiCropperLs.push_back(cropperLs);
 	cropSize++;
+	
+	buildAssemblyLine(assembly, assemblygap, multiCropperLs, croppergap, cropperMulLsBuffer, bgDiff);
 	{
 		std::ofstream svg("output.svg");
 		boost::geometry::svg_mapper<BoostPoint> mapper(svg, 400, 400);
 		mapper.add(bgAssembly);
 		mapper.add(multBGCropper);
+		mapper.add(cropperMulLsBuffer);
 
-		mapper.map(bgAssembly, "fill-opacity:0.5;fill:rgb(153,204,0);stroke:rgb(153,204,0);stroke-width:2");
+		for (int i = 0; i < bgDiff.size(); ++i) {
+			if (bg::within(bgDiff[i], multBGCropper) == false) {
+				mapper.add(bgDiff[i]);
+				mapper.map(bgDiff[i], "fill-opacity:0.5;fill:rgb(153,204,0);stroke:rgb(153,204,0);stroke-width:2");
+			}
+		}
+
+		mapper.map(bgAssembly, "fill-opacity:0.5;fill:rgb(51,153,255);stroke:rgb(0,77,153);stroke-width:2");
 		mapper.map(multBGCropper, "fill-opacity:0.5;fill:rgb(204,153,0);stroke:rgb(202,153,0);stroke-width:2");
+		mapper.map(cropperMulLsBuffer, "fill-opacity:0.5;fill:rgb(255, 255, 153);stroke:rgb(77, 77, 0);stroke-width:2");
 	}
 }
